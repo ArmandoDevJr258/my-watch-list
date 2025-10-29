d34fdb67289079a6472a464176e8ca52394ee825
ArmandoDevJr258 <armando.mabju@gmail.com>
2025-10-29 13:52:55 +0200

    ..

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const settings = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>settings</Text>
     </View>
   )
@@ -13,6 +13,6 @@ export default settings
 
 const styles = StyleSheet.create({
   container:{
-    
+
   }
 })
